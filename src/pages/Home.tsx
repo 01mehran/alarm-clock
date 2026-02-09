d2093012e518129c4f733355f0952c640ca5a845
mehran <mehranmohamadi1311@gmail.com>
2026-02-09 14:46:12 +0330

    feat: implement display current time

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,39 @@
+// Hooks
+import { useEffect, useState } from 'react';
+
 // images
 import clock from '../assets/images/clock.png';
 
 function Home() {
+  const [time, setTime] = useState<string>('');
+
+  //   current time
+  useEffect(() => {
+    const formatTime = (num: number): string => {
+      return num.toString().padStart(2, '0');
+    };
+
+    const updateTime = () => {
+      const date = new Date();
+
+      const hours = formatTime(date.getHours());
+      const minutes = formatTime(date.getMinutes());
+      const seconds = formatTime(date.getSeconds());
+
+      setTime(`${hours} : ${minutes} : ${seconds}`);
+    };
+
+    updateTime();
+
+    const interalTime = setInterval(updateTime, 1000);
+
+    return () => {
+      clearInterval(interalTime);
+    };
+  }, []);
+
   return (
-    <main className="flex h-dvh items-center justify-center bg-blue-400">
+    <main className="small:px-0 flex h-dvh items-center justify-center bg-blue-400 px-2">
       <div className="flex h-110 w-full max-w-120 flex-col items-center justify-around rounded-2xl bg-white p-6">
         <article className="mx-auto w-44">
           <img
@@ -13,12 +43,14 @@ function Home() {
           />
         </article>
 
-        <time className="text-[2.5rem] font-medium">00 : 00 : 00</time>
+        <time className="w-full text-center text-[2.5rem] font-medium">
+          {time || '-- :-- :--'}
+        </time>
 
         <article className="flex w-full space-x-2">
           <div className="control-wrapper">
             <select className="selectEl">
-              <option value="Hour" selected hidden>
+              <option value="" disabled style={{ display: 'none' }}>
                 Hour
               </option>
             </select>
@@ -26,7 +58,7 @@ function Home() {
 
           <div className="control-wrapper">
             <select className="selectEl">
-              <option value="Minutes" selected hidden>
+              <option value="" disabled style={{ display: 'none' }}>
                 Minutes
               </option>
             </select>
