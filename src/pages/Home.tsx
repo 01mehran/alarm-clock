c316e68a6753111fcadad93f20e81ab01aa319c3
mehran <mehranmohamadi1311@gmail.com>
2026-02-10 15:25:12 +0330

    feat: implement handleSetAlarm & play alarm sound whene time is equal selected time

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,23 @@
 // Hooks
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 // images
 import clock from '../assets/images/clock.png';
 
 function Home() {
   const [time, setTime] = useState<string>('');
+  const [selectedHour, setSelectedHour] = useState<string>('');
+  const [selectedMinutes, setSelectedMinutes] = useState<string>('');
 
-  //   current time
+  const [alarmSet, setAlarmSet] = useState(false);
+  const audioRef = useRef<HTMLAudioElement | null>(null);
+
+  const formatTime = (num: number): string => {
+    return num.toString().padStart(2, '0');
+  };
+
+  // current time
   useEffect(() => {
-    const formatTime = (num: number): string => {
-      return num.toString().padStart(2, '0');
-    };
-
     const updateTime = () => {
       const date = new Date();
 
@@ -21,16 +26,29 @@ function Home() {
       const seconds = formatTime(date.getSeconds());
 
       setTime(`${hours} : ${minutes} : ${seconds}`);
+
+      if (
+        alarmSet &&
+        hours === selectedHour &&
+        minutes === selectedMinutes &&
+        seconds === '00'
+      ) {
+        audioRef.current?.play();
+      }
     };
 
-    updateTime();
-
+    // updateTime();
     const interalTime = setInterval(updateTime, 1000);
 
     return () => {
       clearInterval(interalTime);
     };
-  }, []);
+  }, [alarmSet, selectedHour, selectedMinutes]);
+
+  function handleSetAlarm() {
+    if (!selectedHour || !selectedMinutes) return;
+    setAlarmSet(true);
+  }
 
   return (
     <main className="small:px-0 flex h-dvh items-center justify-center bg-blue-400 px-2">
@@ -50,7 +68,11 @@ function Home() {
         <article className="flex w-full space-x-2">
           {/* Hours */}
           <div className="control-wrapper">
-            <select className="selectEl">
+            <select
+              className="selectEl"
+              onChange={(e) => setSelectedHour(e.target.value)}
+              value={selectedHour}
+            >
               <option defaultValue="Minutes" hidden>
                 Hour
               </option>
@@ -64,7 +86,11 @@ function Home() {
 
           {/* Minutes */}
           <div className="control-wrapper">
-            <select className="selectEl">
+            <select
+              className="selectEl"
+              onChange={(e) => setSelectedMinutes(e.target.value)}
+              value={selectedMinutes}
+            >
               <option defaultValue="Minutes" hidden>
                 Minutes
               </option>
@@ -77,9 +103,16 @@ function Home() {
           </div>
         </article>
 
-        <button className="w-full transform cursor-pointer rounded-sm bg-blue-500 py-2 text-xl text-white transition-all duration-200 hover:translate-y-px">
+        <button
+          onClick={handleSetAlarm}
+          className="w-full transform cursor-pointer rounded-sm bg-blue-500 py-2 text-xl text-white transition-all duration-200 hover:translate-y-px"
+        >
           Set alarm
         </button>
+
+        <audio ref={audioRef}>
+          <source src="/ringtone.mp3" typeof="audio/mpeg" />
+        </audio>
       </div>
     </main>
   );
